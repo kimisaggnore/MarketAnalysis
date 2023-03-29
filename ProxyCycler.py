6863bc361908cb969360017ed92a3e3dd7354668
Kimsa <winstonding88@gmail.com>
2023-03-28 21:27:36 -0500

    parallelized webpage retrieval

--- ProxyCycler.py
+++ ProxyCycler.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import aiohttp
+import asyncio
+import time
+
 class ListNode:
     def __init__(self, data):
         self.data = data
@@ -32,6 +36,83 @@ def fetch_price(soup):
     price = price.find_all("bg-quote", {"class": "value"})[0].get_text()
     return price
 
+async def retrieve_price(session, url, proxy):
+    #print(proxy.data)
+    async with session.get(url, proxy = f"http://{proxy}", ssl = False) as res:
+        print("connected")
+        if res.ok:
+            response = await res.text()
+            document_soup = BeautifulSoup(str(response), 'html.parser')
+            price = fetch_price(document_soup)
+            #print(price)
+            return price
+        else:
+            return None
+
+
+async def retrieve_SP_500_prices():
+    proxy = proxies_head
+    async with aiohttp.ClientSession() as session:
+
+        tasks = []
+        for company in all_companies:
+            if proxy.next != None:
+                proxy = proxy.next
+            else:
+                proxy = proxies_head
+            url = f"https://www.marketwatch.com/investing/stock/{company}?mod=search_symbol"
+            tasks.append(asyncio.ensure_future(retrieve_price(session, url, proxy)))
+        
+        all_prices = await asyncio.gather(*tasks)
+        print(all_prices)
+
+all_companies = pd.read_csv("Proxy_Cycling/S&P500-Symbols.csv")
+all_companies = all_companies.loc[:,"Symbol"]
+all_companies = all_companies.tolist()
+all_companies.remove('ABMD')
+all_companies.remove('CTXS')
+all_companies.remove('DRE')
+all_companies.remove('FBHS')
+all_companies.remove('NLSN')
+all_companies.remove('NLOK')
+all_companies.remove('TWTR')
+proxies_head = convert_list_to_linked_list(open("Proxy_Cycling/proxies.txt", "r").read().strip().split("\n"))
+start_time = time.time()
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+asyncio.run(retrieve_SP_500_prices())
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+
+
+
+# start_time = time.time()
+
+
+# async def get_pokemon(session, url):
+#     async with session.get("https://www.marketwatch.com/investing/stock/AAPL?mod=search_symbol") as resp:
+#         pokemon = await resp.text()
+#         return pokemon
+
+
+# async def main():
+
+#     async with aiohttp.ClientSession() as session:
+
+#         tasks = []
+#         for number in range(1, 151):
+#             url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+#             tasks.append(asyncio.ensure_future(get_pokemon(session, url)))
+
+#         original_pokemon = await asyncio.gather(*tasks)
+#         for pokemon in original_pokemon:
+#             print(pokemon)
+
+# asyncio.run(main())
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+
 def retrieve_all_prices():
     prices_list = []
     all_companies = pd.read_csv("Proxy_Cycling/S&P500-Symbols.csv")
@@ -46,7 +127,7 @@ def retrieve_all_prices():
     all_companies.remove('TWTR')
     proxies_head = convert_list_to_linked_list(open("Proxy_Cycling/proxies.txt", "r").read().strip().split("\n"))
     proxy = proxies_head
-    all_companies = all_companies[450:]
+    # all_companies = all_companies[450:]
     for company in all_companies:
         if proxy != None:
             url = f"https://www.marketwatch.com/investing/stock/{company}?mod=search_symbol"
@@ -61,36 +142,13 @@ def retrieve_all_prices():
             #print(num_cycles)
             document_soup = BeautifulSoup(str(res), 'html.parser')
             #print(fetch_price(document_soup))
-            prices_list.append(fetch_price(document_soup))
+            cur_price = fetch_price(document_soup)
+            prices_list.append(cur_price)
+            print(cur_price)
             proxy = proxy.next
         else:
             proxy = proxies_head
-  
-        print(company)
-    return prices_list
 
+    #return prices_list
 
-retrieve_all_prices()
-# num_cycles = 1
-# proxies_head = convert_list_to_linked_list(open("Proxy_Cycling/proxies.txt", "r").read().strip().split("\n"))
-# proxy = proxies_head
-# test_url = "https://www.marketwatch.com/investing/stock/enph?mod=search_symbol"
-# res = get_page(test_url, proxy)
-# print(num_cycles)
-# while not res[0]:
-#     proxy = proxy.next
-#     if proxy == None:
-#         break
-#     res = get_page(test_url, proxy)
-#     num_cycles += 1
-#     print(num_cycles)
-
-
-# document_soup = BeautifulSoup(str(res), 'html.parser')
-
-# print(fetch_price(document_soup))
-
-
-#[0].find_all("tr", {"class": "table__row"})
-
-#print(proxies)
+#retrieve_all_prices()
