fdfdfc76173abc92e671d59fe76403ced2015e98
Kimsa <winstonding88@gmail.com>
2023-04-02 22:41:04 -0500

    factored metric analysis code into functions

--- Company_Financial_Scrape/company_metric_analysis.py
+++ Company_Financial_Scrape/company_metric_analysis.py
@@ -16,44 +16,48 @@ healthy_companies = []
 metric_dict_less = {1:15, 4:4, 5:3, 6:14, 28:50, 29:40} #, 4:4, 5:3, 6:14, 28:50, 29:40
 metric_dict_more = {14: 1.5, 20:20, 21:10,  22:12, 23:20, 24:20} #14: 1.5, 20:20, 21:10,  22:12, 23:20, 24:20
 
-with open(os.path.join(sys.path[0],'company_profiles/company_fundamental_valuation_2.csv'), mode = "r", newline = '') as company_fundamental_valuation:
-    with open(os.path.join(sys.path[0],'company_profiles/company_fundamental_valuation_copy.csv'), mode = "w", newline = '') as company_fundamental_valuation_write:
-        company_fundamental_valuation_reader = list(csv.reader(company_fundamental_valuation, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL))
-        company_fundamental_valuation_writer = csv.writer(company_fundamental_valuation_write, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-        for company in company_fundamental_valuation_reader:
-            copied_row = []
-            for cell in company:
-                cell = cell.replace('%', '')
-                cell = cell.replace(',', '')
-                copied_row.append(cell)
-            company_fundamental_valuation_writer.writerow(copied_row)
+def refactor_metric_data():
+    with open(os.path.join(sys.path[0],'company_profiles/company_fundamental_valuation_2.csv'), mode = "r", newline = '') as company_fundamental_valuation:
+        with open(os.path.join(sys.path[0],'company_profiles/company_fundamental_valuation_copy.csv'), mode = "w", newline = '') as company_fundamental_valuation_write:
+            company_fundamental_valuation_reader = list(csv.reader(company_fundamental_valuation, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL))
+            company_fundamental_valuation_writer = csv.writer(company_fundamental_valuation_write, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
+            for company in company_fundamental_valuation_reader:
+                copied_row = []
+                for cell in company:
+                    cell = cell.replace('%', '')
+                    cell = cell.replace(',', '')
+                    copied_row.append(cell)
+                company_fundamental_valuation_writer.writerow(copied_row)
 
-with open(os.path.join(sys.path[0],'company_profiles/company_fundamental_valuation_copy.csv'), mode = "r", newline = '') as company_fundamental_valuation:
-    company_fundamental_valuation_reader = csv.reader(company_fundamental_valuation, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-    next(company_fundamental_valuation_reader)
-    for company in company_fundamental_valuation_reader:
-        if company[0] != "N/A":
-            track_metric = [0]*12
-            count = 0
-            #print(company[0])
-            for metric in metric_dict_less:
-                if company[metric] != "N/A" and float(company[metric]) <= metric_dict_less[metric]:
-                    track_metric[count] = True
-                else: 
-                    track_metric[count] = False
-                count += 1
-                
-            for metric in metric_dict_more:
-                if company[metric] != "N/A"  and float(company[metric]) >= metric_dict_more[metric]:
-                    track_metric[count] = True
-                else: 
-                    track_metric[count] = False
-                count += 1
-            if np.count_nonzero(track_metric) >= (4/8)*len(track_metric):
-                healthy_companies.append(company[0])
-            #print(track_metric)
+def fit_metrics_to_companies():
+    with open(os.path.join(sys.path[0],'company_profiles/company_fundamental_valuation_copy.csv'), mode = "r", newline = '') as company_fundamental_valuation:
+        company_fundamental_valuation_reader = csv.reader(company_fundamental_valuation, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
+        next(company_fundamental_valuation_reader)
+        for company in company_fundamental_valuation_reader:
+            if company[0] != "N/A":
+                track_metric = [0]*12
+                count = 0
+                #print(company[0])
+                for metric in metric_dict_less:
+                    if company[metric] != "N/A" and float(company[metric]) <= metric_dict_less[metric]:
+                        track_metric[count] = True
+                    else: 
+                        track_metric[count] = False
+                    count += 1
+                    
+                for metric in metric_dict_more:
+                    if company[metric] != "N/A"  and float(company[metric]) >= metric_dict_more[metric]:
+                        track_metric[count] = True
+                    else: 
+                        track_metric[count] = False
+                    count += 1
+                if np.count_nonzero(track_metric) >= (4/8)*len(track_metric):
+                    healthy_companies.append(company[0])
+                #print(track_metric)
+    return healthy_companies
         
 
-print(healthy_companies)
+#refactor_metric_data()
+print(fit_metrics_to_companies())
 
             
